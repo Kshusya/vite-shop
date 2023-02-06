56fa902a5faf62cc4ff0deefd4fa7998650f9f90
Ksenia Peresedova <kshusya.yay@gmail.com>
2023-02-07 03:18:33 +0400

    add more products

--- src/products.js
+++ src/products.js
@@ -6,23 +6,27 @@ export default [
     },
     {
         id: 2,
-        name: "Bratz doll Kiana",
-        price: 23.99,
+        name: "Bratz doll Nevra",
+        price: 20.99,
     },
     {
         id: 3,
         name: "Bratz doll Kumi",
-        price: 23.99,
+        price: 20.99,
     },
     {
         id: 4,
-        name: "Bratz doll Cloe",
-        price: 23.99,
+        name: "Bratz doll Kiana",
+        price: 17.99,
     },
     {
         id: 5,
-        name: "Bratz doll Sasha",
-        price: 23.99,
+        name: "Bratz doll Yasmin",
+        price: 48.00,
     },
-
+    {
+        id: 6,
+        name: "Bratz doll Sasha",
+        price: 24.99,
+    },
 ]
