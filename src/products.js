8f89341cb13ab327b501cbbb6425785d346be39d
Ksenia Peresedova <kshusya.yay@gmail.com>
2023-02-13 06:07:08 +0400

    updated data

--- src/products.js
+++ src/products.js
@@ -1,33 +1,43 @@
 const products = [
     {
         id: 1,
-        name: "Bratz doll Meygan",
+        name: "Bratz Original Fashion Doll Meygan",
         price: 23.99,
     },
     {
         id: 2,
-        name: "Bratz doll Nevra",
+        name: "Bratz Original Fashion Doll Nevra",
         price: 20.99,
     },
     {
         id: 3,
-        name: "Bratz doll Kumi",
+        name: "Bratz Original Fashion Doll Kumi",
         price: 20.99,
     },
     {
         id: 4,
-        name: "Bratz doll Kiana",
+        name: "Bratz Original Fashion Doll Kiana",
         price: 17.99,
     },
     {
         id: 5,
-        name: "Bratz doll Yasmin",
+        name: "Bratz 20 Yearz Special Anniversary Edition Original Fashion Doll Yasmin",
         price: 48.00,
     },
     {
         id: 6,
-        name: "Bratz doll Sasha",
+        name: "Bratz 20 Yearz Special Anniversary Edition Original Fashion Doll Sasha",
         price: 24.99,
+    },
+    {
+        id: 7,
+        name: "Bratz 20 Yearz Special Anniversary Edition Original Fashion Doll Cloe",
+        price: null,
+    },
+    {
+        id: 8,
+        name: "Bratz 20 Yearz Special Anniversary Edition Original Fashion Doll Jade",
+        price: 44.84,
     },
 ]
 
