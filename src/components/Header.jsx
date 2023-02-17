2040aa47008e4a529e4d5ec13f12f82afaa6ae6b
Ksenia Peresedova <kshusya.yay@gmail.com>
2023-02-17 17:50:57 +0400

    added and styled Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { UserContext } from "../App"
 import { useContext } from "react"
 import { getProductById } from "../products"
+import { Link } from "react-router-dom"
 
 export default function Header() {
-    const { cart, setCart } = useContext(UserContext)
+    const { cart } = useContext(UserContext)
 
     let totalPrice = 0
     cart.forEach(cartItemId => {
@@ -12,8 +13,20 @@ export default function Header() {
     })
 
     return (
-        <div>
-            Products in cart: {cart.length}, total price: {totalPrice.toFixed(2)}
+        <div className="header">
+            <Link to="/"><h1 className="header__logo">Doll Shop</h1></Link>
+            <div className="header__cart-value cart-value">
+                <div>
+                    <img className="cart-value__cart-icon" width="50" height="50" src="/cart-basket-ui-5-svgrepo-com.svg" alt="" />
+                    <div className="cart-value__number-of-products">{cart.length}</div>
+                </div>
+                <div className="cart-value__total-price">
+                    {totalPrice.toFixed(2)}$
+                </div>
+                {/* <div>
+                    Products in cart: <div className="cart-value__number-of-products">{cart.length}</div>
+                </div> */}
+            </div>
         </div>
     )
 }
